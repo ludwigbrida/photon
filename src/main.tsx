5f6fd0a3f78bbb77782088606d6025b24ee5e41f
Ludwig Brida <ludwigbrida@gmail.com>
2024-01-07 23:00:21 +0100

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {App} from './app.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const main = ReactDOM.createRoot(
+  document.getElementById("main") as HTMLElement,
+);
+main.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
