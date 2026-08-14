06cd8a89c2d9bedcf6e92ba8f72d7f0926f78199
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-08-14 14:24:45 +0200

    chore: create sandbox package

---
+++ apps/sandbox/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./main.css";
+
+const main = document.getElementById("main") as HTMLElement;
+
+const root = createRoot(main);
+
+root.render(
+  <StrictMode>
+    <canvas />
+  </StrictMode>,
+);
