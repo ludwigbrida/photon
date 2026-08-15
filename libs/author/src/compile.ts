3b6c818061e4f5a68ae72dbea7806665d3f41214
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-08-15 22:24:52 +0200

    feat: add materials to compiled scene

--- libs/author/src/compile.ts
+++ libs/author/src/compile.ts
@@ -3,6 +3,7 @@ import { Voxel } from "./types.ts";
 
 export type CompiledScene = {
   readonly octree: Uint32Array;
+  readonly materials: Float32Array;
 };
 
 export const compile = (root: Voxel): CompiledScene => {
@@ -10,15 +11,18 @@ export const compile = (root: Voxel): CompiledScene => {
 
   const childIndex = x | (y << 1) | (z << 2);
 
-  const nodes = new Uint32Array(9);
+  const octree = new Uint32Array(9);
 
-  nodes[0] = encodeBranch(1);
+  octree[0] = encodeBranch(1);
 
   for (let i = 0; i < 8; i++) {
-    nodes[i + 1] = i === childIndex ? encodeLeaf(0) : encodeEmpty();
+    octree[i + 1] = i === childIndex ? encodeLeaf(0) : encodeEmpty();
   }
 
+  const materials = new Float32Array([root.color[0], root.color[1], root.color[2], 1]);
+
   return {
-    octree: nodes,
+    octree,
+    materials,
   };
 };
