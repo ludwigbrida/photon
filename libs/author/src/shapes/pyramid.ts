6d44ad5a7c2bb44807bf8cb9a70fdfc2d9353f1d
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-08-26 23:56:45 +0200

    feat: create pyramid shape

---
+++ libs/author/src/shapes/pyramid.ts
@@ -0,0 +1,30 @@
+import { Material } from "../material.ts";
+import { Shape } from "../types/shape.ts";
+
+export type PyramidOptions = {
+  readonly height: number;
+  readonly material: Material;
+};
+
+export const pyramid = ({ height, material }: PyramidOptions): Shape => {
+  const baseHalfSize = height - 1;
+
+  return {
+    bounds: {
+      min: [-baseHalfSize, 0, -baseHalfSize],
+      max: [baseHalfSize + 1, height, baseHalfSize + 1],
+    },
+    sample: (x, y, z) => {
+      if (y < 0 || y >= height) {
+        return undefined;
+      }
+
+      const halfSizeAtY = baseHalfSize - y;
+
+      const isInside =
+        x >= -halfSizeAtY && x <= halfSizeAtY && z >= -halfSizeAtY && z <= halfSizeAtY;
+
+      return isInside ? material : undefined;
+    },
+  };
+};
