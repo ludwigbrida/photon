ad4a6816cfe9aec68ac4d9a6a0c9701ffe1516cd
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-08-26 21:21:54 +0200

    refactor: rename geometry type to shape

--- libs/author/src/types/shape.ts
+++ libs/author/src/types/shape.ts
@@ -1,7 +1,7 @@
 import { type Material } from "../material.ts";
 import { type Bounds } from "./bounds.ts";
 
-export type Geometry = {
+export type Shape = {
   readonly bounds: Bounds;
   readonly sample: (x: number, y: number, z: number) => Material | undefined;
 };
