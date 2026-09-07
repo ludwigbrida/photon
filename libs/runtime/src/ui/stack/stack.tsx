425e3175de69d48f50459ddfac04594b64b12c7d
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-07 21:45:27 +0200

    feat: use separator in stack component

--- libs/runtime/src/ui/stack/stack.tsx
+++ libs/runtime/src/ui/stack/stack.tsx
@@ -1,4 +1,5 @@
 import { Fragment, type ReactNode } from "react";
+import { Separator } from "../separator/separator.tsx";
 import styles from "./stack.module.css";
 
 type StackProps = {
@@ -9,7 +10,7 @@ export const Stack = ({ items }: StackProps) => (
   <div className={styles.stack}>
     {items.map((item, index) => (
       <Fragment key={index}>
-        {index > 0 && <span className={styles.divider} />}
+        {index > 0 && <Separator />}
         {item}
       </Fragment>
     ))}
