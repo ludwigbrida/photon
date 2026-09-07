e67cf644cba6cc5e2643c3ba9a27b257cea5787b
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-07 23:28:56 +0200

    refactor: rename field component to metric

--- libs/runtime/src/ui/metric/metric.tsx
+++ libs/runtime/src/ui/metric/metric.tsx
@@ -1,12 +1,12 @@
 import type { PropsWithChildren } from "react";
-import styles from "./field.module.css";
+import styles from "./metric.module.css";
 
-type FieldProps = PropsWithChildren<{
+type MetricProps = PropsWithChildren<{
   label: string;
   unit?: string;
 }>;
 
-export const Field = ({ label, children, unit }: FieldProps) => (
+export const Metric = ({ label, children, unit }: MetricProps) => (
   <div>
     <span className={styles.label}>{label}</span> <span className={styles.value}>{children}</span>
     <span className={styles.label}>{unit}</span>
