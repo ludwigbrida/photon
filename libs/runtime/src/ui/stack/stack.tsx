3142c1480c75bbc206f44c204fb9b0200ad28a07
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-07 23:50:03 +0200

    feat: add orientation to stack layout

--- libs/runtime/src/ui/stack/stack.tsx
+++ libs/runtime/src/ui/stack/stack.tsx
@@ -1,13 +1,20 @@
+import clsx from "clsx";
 import { Fragment, type ReactNode } from "react";
 import { Separator } from "../separator/separator.tsx";
 import styles from "./stack.module.css";
 
 type StackProps = {
+  orientation?: "horizontal" | "vertical";
   children: ReactNode[];
 };
 
-export const Stack = ({ children }: StackProps) => (
-  <div className={styles.stack}>
+export const Stack = ({ orientation = "horizontal", children }: StackProps) => (
+  <div
+    className={clsx(styles.stack, {
+      [styles.horizontal]: orientation === "horizontal",
+      [styles.vertical]: orientation === "vertical",
+    })}
+  >
     {children.map((item, index) => (
       <Fragment key={index}>
         {index > 0 && <Separator />}
